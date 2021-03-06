a9a4d47a21ee21ac3d308ba6e34314626892d9e0
uushusni <uushusni3@gmail.com>
2021-03-06 20:28:49 +0700

    prefix

--- commands/list.js
+++ commands/list.js
@@ -7,14 +7,14 @@ module.exports = {
             .setTitle('LIST SCRIPT')
             .setThumbnail(message.author.displayAvatarURL())
             .addFields(
-                {name: '1', value: 'arsenal(-arsenal)'},
-                {name: '2', value: 'BloxFruit(-bf)'},
-                {name: '3', value: 'RO-GHOUL(-ghoul)'},
-                {name: '4', value: 'Universal Time(-jojo)'},
-                {name: '5', value: 'KingPiece(-kp)'},
-                {name: '6', value: 'ProjectX(-prox)'},
-                {name: '7', value: 'ShindoLife(-shindo)'},
-                {name: '8', value: 'Wisteria(-wisteria)'}
+                {name: '1', value: 'arsenal(,arsenal)'},
+                {name: '2', value: 'BloxFruit(,bf)'},
+                {name: '3', value: 'RO-GHOUL(,ghoul)'},
+                {name: '4', value: 'Universal Time(,jojo)'},
+                {name: '5', value: 'KingPiece(,kp)'},
+                {name: '6', value: 'ProjectX(,prox)'},
+                {name: '7', value: 'ShindoLife(,shindo)'},
+                {name: '8', value: 'Wisteria(,wisteria)'}
             )
             .setThumbnail(message.author.displayAvatarURL())
             .setTimestamp()
