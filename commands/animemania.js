a9bb80c890f0592a36f924c0cd6abf6ca10bb4bf
uushusni <58155830+uushusni@users.noreply.github.com>
2021-04-13 02:14:51 +0700

    asdasdasdasd

---
+++ commands/animemania.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+module.exports = {
+    name: 'anm',
+    description: "Embeds",
+    execute(message, args, client) {
+        const embed = new Discord.MessageEmbed()
+            
+            message.author.send(embed,{files: ["./commands/animemania.lua"]});
+            const embed1 = new Discord.MessageEmbed()
+            .setColor(0x6509ed)
+            .setTitle('Reds_pirates')
+            .setThumbnail(message.author.username)
+            .setDescription('ANIME MANIA')
+            .addField('Check Youre DM FOR Script', 'DENIM IS GOD')
+            .setThumbnail(message.author.displayAvatarURL())
+            .setTimestamp()
+            message.channel.send(embed1);
+        
+        
+    }
+}
---
+++ commands/animemania.lua
@@ -0,0 +1,6 @@
+_G.Level = "Maruto Unlimited Mode" -- Map Selection ["Leaf Village Gate", "Bandit Raid", "Chunins Exam", "Sand Raid", "Maruto Unlimited Mode", "Hollow Raid", "Corrupt Soul Reaper Ambush", "Vasto Raid", "Arrankar Invasion", "Lord Aiden", "Reaper Unlimited Mode"]
+_G.Password = "SomePassword" -- set password to w/e, not sure how multiple people using same password works
+_G.Distance = "8" -- Distance under while farming (8 is default, change as you like)
+_G.AttackType = "Spin" -- "Spin" will spin around under them, default will just be under them.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Viper123321/anime-mania-autofarm/main/script"))()
