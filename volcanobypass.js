eb40ab8af327536204019441fa0724ecef8ca3cb
denimfelix <58155830+uushusni@users.noreply.github.com>
2025-12-01 18:29:09 +0700

    Update volcanobypass.js
    fix latest

--- volcanobypass.js
+++ volcanobypass.js
@@ -779,7 +779,7 @@
                 let waitTimeSeconds = 5;
                 const url = location.href;
                 if (url.includes('42rk6hcq') || url.includes('ito4wckq') || url.includes('pzarvhq1')) {
-                    waitTimeSeconds = 30;
+                    waitTimeSeconds = 20;
                 }
 
                 if (secondsPassed >= waitTimeSeconds) {
